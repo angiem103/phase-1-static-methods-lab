1224b68a6c1819b123f5f4bd0ffbcd3046738f05
Angie <amaticorena103@hotmail.com>
2021-12-26 14:22:16 -0500

    commit tree ea8c61e0b5895084ad3989c813d99ec793612d38

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9' -]+/g, '')
+  }
+  static titleize(string){
+    let excludedWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const cap = string => string.charAt(0).toUpperCase() + string.slice(1);
+    return cap(
+      string.replace(/\b\w+\b/g, m => excludedWords.includes(m) ? m : cap(m))
+    );
+  }
 }
+
+//that takes in a string and capitalizes all words in a sentence except the, a, an, but, of, and, for, at, by, and from
